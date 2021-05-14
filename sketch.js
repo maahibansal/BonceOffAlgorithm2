959576838d8d087f75bad20650e42f96df127063
maahibansal <78861817+maahibansal@users.noreply.github.com>
2021-05-14 16:53:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+  var fixedRect, movingRect;
+  var gameObject1, gameObject2, gameObject3, gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  gameObject1 = createSprite(100, 100, 50, 50);
+  gameObject1.shapeColor = "green";
+  gameObject2 = createSprite(200, 100, 50, 50);
+  gameObject2.shapeColor = "green";
+  gameObject3 = createSprite(300, 100, 50, 50);
+  gameObject3.shapeColor = "green";
+  gameObject4 = createSprite(400, 100, 50, 50);
+  gameObject4.shapeColor = "green";
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);
+
+  bounceOff(movingRect,fixedRect)
+  
+  drawSprites();
+}
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+      && object2.x - object1.x < object2.width/2 + object1.width/2) {
+    object1.velocityX = object1.velocityX * (-1);
+    object2.velocityX = object2.velocityX * (-1);
+  }
+  if (object1.y - object2.y < object2.height/2 + object1.height/2
+    && object1.y - object1.y < object2.height/2 + object1.height/2){
+    object1.velocityY = object1.velocityY * (-1);
+    object2.velocityY = object2.velocityY * (-1);
+  }
+}
